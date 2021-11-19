7deb8ece6b7370168f1f0f3ede34ae233320b0b2
SebGoll <50230837+SebGoll@users.noreply.github.com>
2021-11-19 09:18:30 +0100

    SaveAndLoad

--- src/stream/ClientThread.java
+++ src/stream/ClientThread.java
@@ -21,13 +21,13 @@ public class ClientThread
     private Socket clientSocket;
     public boolean messageSent;
     public SharedData sd;
-    public String nom;
+    public String name;
 
 
     ClientThread(Socket s, SharedData data) {
         this.clientSocket = s;
         this.sd = data;
-        this.nom = null;
+        this.name = null;
     }
 
     /**
@@ -40,16 +40,17 @@ public class ClientThread
                     new InputStreamReader(clientSocket.getInputStream()));
             PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
             socOut.println("Renseignez votre pseudonyme");
-            nom = socIn.readLine();
-            socOut.println("Bienvenue "+nom+" vous pouvez maintenant chatter avec vos amis !");
+            name = socIn.readLine();
+            socOut.println("Bienvenue "+ name +" vous pouvez maintenant chatter avec vos amis !\n");
+            Persistence.logAndLoad(name,socOut,0);
 
 
             while (true) {
                 if (socIn.ready()) {
                     String line = socIn.readLine();
                     messageSent = true;
-                    sd.messagesToSend.add(new AbstractMap.SimpleEntry<>(line, nom));
-                    Persistence.persist(line,0);
+                    sd.messagesToSend.add(new AbstractMap.SimpleEntry<>(line, name));
+                    Persistence.persist(line,this.name,0);
                 }
                 if (sd.messageSent.get(this.getId())) {
                     socOut.println(sd.messagesToSend.get(0).getValue() + " : " + sd.messagesToSend.get(0).getKey());
--- src/stream/Persistence.java
+++ src/stream/Persistence.java
@@ -1,21 +1,56 @@
 package stream;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.SQLOutput;
+import java.io.*;
+import java.util.Scanner;
 
 public class Persistence {
-    public static void persist(String message, long idConv){
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BOLD = "\u001B[1m";
 
-        try{
-            File persistenceFile = new File("PersistenceData/PersistenceData_"+idConv+".");
-            if(persistenceFile.createNewFile()){
-                System.out.println("New persistence file created for group chat "+idConv+".");
-            }else{
-                System.out.println("Persistence file for group chat "+idConv+" was located.");
+    public static void logAndLoad(String name, PrintStream out, long idConv) {
+        try {
+            File persistenceFile = new File("PersistenceData/PersistenceData_" + idConv);
+            if (persistenceFile.createNewFile()) {
+                System.out.println("New persistence file created for group chat " + idConv + ".");
+            } else {
+                System.out.println("Persistence file for group chat " + idConv + " was located.");
             }
-        }catch(IOException e){
-            System.out.println("An error as occured.");
+        } catch (IOException e) {
+            System.out.println("An error as occurred when opening/creating the file.");
+            e.printStackTrace();
+        }
+        try {
+            Scanner reader = new Scanner(new File("PersistenceData/PersistenceData_" + idConv));
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+
+                String[] data = line.split(":", 2);
+                if (data[0].equals(name)) {
+                    out.println(ANSI_GREEN + data[1] + ANSI_RESET);
+                } else {
+                    out.println(ANSI_BOLD + data[0] + " : " + ANSI_RESET + data[1]);
+                }
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error as occurred while reading the file.");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void persist(String message, String name, long idConv) {
+
+
+        try {
+            FileWriter writerToPersistenceFile = new FileWriter("PersistenceData/PersistenceData_" + idConv, true);
+            writerToPersistenceFile.write(name + ":" + message);
+            writerToPersistenceFile.write('\n');
+            writerToPersistenceFile.close();
+            System.out.println("Message successfully persisted.");
+        } catch (IOException e) {
+            System.out.println("An error as occurred when persisting the message.");
             e.printStackTrace();
         }
 
